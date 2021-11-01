0cb873fc079fb3c4d96b41cbd96b6354bb71a3d4
asui02 <uhaato@gmail.com>
2021-11-01 11:38:44 +0900

    Done

---
+++ app/src/main/java/com/example/test/Second.java
@@ -0,0 +1,12 @@
+package com.example.test;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Second extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
+
